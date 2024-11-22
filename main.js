e1ef9e14c854a198d6071939b7b306be1c29d844
Andrei Stroianetki <sandreasweb@gmail.com>
2024-11-22 10:52:17 +0100

    update style

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const dialogElem = document.querySelector(".dialog_elem");
-const backroup = document.querySelector("#dialog_container");
-const addBtn = document.querySelector(".add_list_btn");
+const dialogElem = document.querySelector('.dialog_elem');
 
-addBtn.addEventListener("click", () => {
-  backroup.classList.add("backroup");
-  dialogElem.show();
-  console.log("click");
-  
-});
+const addBtn = document.querySelector('.add_list_btn');
+
+addBtn.addEventListener('click', () => { 
+    dialogElem.showModal()
+})
+
