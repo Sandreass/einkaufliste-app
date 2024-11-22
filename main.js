a674711f3b2841341f60c6efd001a6e11151c4e6
Andrei Stroianetki <sandreasweb@gmail.com>
2024-11-22 11:12:25 +0100

    close dialog done

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const dialogElem = document.querySelector('.dialog_elem');
+const dialogElem = document.querySelector(".dialog_elem");
 
-const addBtn = document.querySelector('.add_list_btn');
+const addBtn = document.querySelector(".add_list_btn");
 
-addBtn.addEventListener('click', () => { 
-    dialogElem.showModal()
-})
+const closeBtn = document.querySelector(".close_dialog");
 
+addBtn.addEventListener("click", () => {
+  dialogElem.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  dialogElem.close();
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === dialogElem) {
+    dialogElem.close();
+  }
+});
