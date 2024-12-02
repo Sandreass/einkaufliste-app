c76d001606978df2c337336f86f11b5cbf52f2f1
Andrei Stroianetki <sandreasweb@gmail.com>
2024-12-02 14:48:54 +0100

    update menge funktion

--- main.js
+++ main.js
@@ -1,167 +1,181 @@
-const dialogElem = document.querySelector(".dialog_elem");
-const addBtn = document.querySelector(".add_list_btn");
-const closeBtn = document.querySelector(".close_dialog");
-const formElem = document.querySelector("form");
+const dialogElem = document.querySelector('.dialog_elem');
+const addBtn = document.querySelector('.add_list_btn');
+const closeBtn = document.querySelector('[data-close-btn]');
+const formElem = document.querySelector('form');
 const inputPreis = document.querySelector('input[name="preis"]');
-const h4Elem = document.createElement("h4");
-const sortMengeButton = document.querySelector(".sort_menge");
-const productListElem = document.getElementById("product-list");
+const h4Elem = document.createElement('h4');
+const rootElem = document.querySelector('.root');
+const sortMengeBtn = document.querySelector('.sort_menge');
+const sortNameBtn = document.querySelector('.sort_name');
+
+const iconAsc = document.querySelector('.sort_asc');
+const iconDesc = document.querySelector('.sort_desc');
+
 
 dialogElem.insertBefore(h4Elem, dialogElem.firstChild);
 
 let isEditing = false;
 let currentEditId = null;
-let isMengeAsc = true; 
+let sortDirection = 'asc';
 
-document.addEventListener("DOMContentLoaded", () => {
-  renderProducts();
+document.addEventListener('DOMContentLoaded', () => {
+    renderProducts();
+})
+
+inputPreis.addEventListener('input', (e) => {
+    const inputPriceValue = e.target.value;
+    let msgElem = e.target.nextElementSibling;
+
+    if (msgElem) {
+        msgElem.remove()
+    }
+    if (isNaN(inputPriceValue) || inputPriceValue < 1) {
+        msgElem = document.createElement('p');
+        msgElem.classList.add('msg');
+        msgElem.textContent = 'Preis muss größer als 1 sein!';
+        msgElem.style.color = 'red'; // Style für die Fehlermeldung
+        msgElem.style.fontSize = '0.9rem';
+        msgElem.style.padding = '0.4rem 0';
+
+        e.target.parentElement.appendChild(msgElem); // Nachricht direkt nach dem Input einfügen
+    } else if (inputPriceValue === '') {
+        msgElem = document.createElement('p');
+        msgElem.classList.add('msg');
+        msgElem.textContent = 'Dieses Feld darf nicht leer sein!';
+        e.target.parentElement.appendChild(msgElem);
+    }
+})
+
+addBtn.addEventListener('click', () => {
+    isEditing = false;
+    formElem.reset()
+    dialogElem.showModal();
+
+    h4Elem.textContent = 'Produckt hinzufügen'
+
 });
 
-inputPreis.addEventListener("input", (e) => {
-  const inputPriceValue = e.target.value;
-  let msgElem = e.target.nextElementSibling;
-
-  if (msgElem) {
-    msgElem.remove();
-  }
-  if (isNaN(inputPriceValue) || inputPriceValue < 1) {
-    msgElem = document.createElement("p");
-    msgElem.classList.add("msg");
-    msgElem.textContent = "Preis muss größer als 1 sein!";
-    msgElem.style.color = "red";
-    msgElem.style.fontSize = "0.9rem";
-    msgElem.style.padding = "0.4rem 0";
-
-    e.target.parentElement.appendChild(msgElem);
-  } else if (inputPriceValue === "") {
-    msgElem = document.createElement("p");
-    msgElem.classList.add("msg");
-    msgElem.textContent = "Dieses Feld darf nicht leer sein!";
-    e.target.parentElement.appendChild(msgElem);
-  }
+closeBtn.addEventListener('click', () => {
+    dialogElem.close()
 });
 
-addBtn.addEventListener("click", () => {
-  isEditing = false;
-  formElem.reset();
-  dialogElem.showModal();
+// window.addEventListener('click', (event) => {
+//     if (event.target === dialogElem) {
+//         dialogElem.close()
+//     }
+// });
 
-  h4Elem.textContent = "Produkt hinzufügen";
-});
+formElem.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-closeBtn.addEventListener("click", () => {
-  dialogElem.close();
-});
+    const { name, menge, preis } = e.target.elements;
 
-formElem.addEventListener("submit", (e) => {
-  e.preventDefault();
+    const nameValue = name.value.trim();
+    const formattedName = nameValue.charAt(0).toUpperCase() + nameValue.slice(1).toLowerCase();
+    const mengeValue = parseFloat(menge.value.trim());
+    const preisValue = parseFloat(preis.value.trim());
 
-  const { name, menge, preis } = e.target.elements;
+    const exitingData = getDataFromLocaleStorage();
 
-  const nameValue = name.value.trim();
-  const formattedName =
-    nameValue.charAt(0).toUpperCase() + nameValue.slice(1).toLowerCase();
-  const mengeValue = parseFloat(menge.value.trim());
-  const preisValue = parseFloat(preis.value.trim());
+    if (isEditing && currentEditId !== null) {
+        const updatedEditedData = exitingData.map(product => {
+            return product.id === currentEditId ? { ...product, name: formattedName, menge: mengeValue, preis: preisValue } : product;
 
-  const existingData = getDataFromLocaleStorage();
 
-  if (isEditing && currentEditId !== null) {
-    const updatedEditedData = existingData.map((product) => {
-      return product.id === currentEditId
-        ? {
-            ...product,
+        })
+        
+        saveToLocaleStorage(updatedEditedData)
+
+    } else {
+        const newData =
+        {
             name: formattedName,
             menge: mengeValue,
             preis: preisValue,
-          }
-        : product;
-    });
+            id: new Date().getTime()
+        }
 
-    saveToLocaleStorage(updatedEditedData);
-  } else {
-    const newData = {
-      name: formattedName,
-      menge: mengeValue,
-      preis: preisValue,
-      id: new Date().getTime(),
-    };
+        const updatedDate = [...exitingData, newData];
+        saveToLocaleStorage(updatedDate)
+    }
 
-    const updatedData = [...existingData, newData];
-    saveToLocaleStorage(updatedData);
-  }
-
-  formElem.reset();
-  dialogElem.close();
-  renderProducts();
-});
+    formElem.reset()
+    // name.value = '';
+    // menge.value = '';
+    // preis.value = '';
+    dialogElem.close()
+    renderProducts()
+})
 
 const saveToLocaleStorage = (dataObj) => {
-  localStorage.setItem("Products", JSON.stringify(dataObj));
+    localStorage.setItem('Products', JSON.stringify(dataObj))
 };
 
 const getDataFromLocaleStorage = () => {
-  const data = localStorage.getItem("Products");
-  return data ? JSON.parse(data) : [];
+    const data = localStorage.getItem('Products');
+    return data ? JSON.parse(data) : [];
 };
 
-const renderProducts = () => {
-  const products = getDataFromLocaleStorage();
-  productListElem.innerHTML = `
-    <ul class='lists_container'>
-      ${products
-        .map(
-          (product) => `
-        <li class='list_card'>
-          <p><strong>${product.menge} ${product.name}</strong></p>
-          <div class="btn_container">
-            <span onclick="editProduct(${product.id})"><i class="fa-regular fa-pen-to-square"></i></span>
-            <span onclick="deleteProduct(${product.id})"><i class="fa-regular fa-trash-can"></i></span>
-          </div>
-        </li>
-      `
-        )
-        .join("")}
-    </ul>
-  `;
-};
+const renderProducts = (products=null) => {
+    const productsToARender=products||getDataFromLocaleStorage()
+
+    rootElem.innerHTML = `
+<ul class='lists_container'>
+${productsToARender.map(product => `
+    <li class='list_card'>
+    <p><strong>${product.menge} ${product.name}</strong></p>
+    <div class="btn_container">
+    <span onclick="editProduct(${product.id})"><i class="fa-regular fa-pen-to-square"></i></span>
+    <span onclick="deleteProduct(${product.id})"><i class="fa-regular fa-trash-can"></i></span>
+    </div>
+    </li>
+    `).join('')}
+</ul>
+`
+}
 
 const editProduct = (id) => {
-  h4Elem.textContent = "Product bearbeiten";
-
   const products = getDataFromLocaleStorage();
-
-  const productToEdit = products.find((product) => product.id === id);
+  
+  const productToEdit = products.find(product => product.id === id);
 
   if (productToEdit) {
-    isEditing = true;
-    currentEditId = id;
+      isEditing = true;
+      currentEditId = id
 
-    const { name, menge, preis } = formElem.elements;
+      const { name, menge, preis } = formElem.elements;
 
-    name.value = productToEdit.name;
-    menge.value = productToEdit.menge;
-    preis.value = productToEdit.preis;
+      name.value = productToEdit.name;
+      menge.value = productToEdit.menge;
+      preis.value = productToEdit.preis
 
-    dialogElem.showModal();
+      dialogElem.showModal();
+
+      h4Elem.innerText = 'Product bearbeiten';
   }
-};
+}
 
 const deleteProduct = (id) => {
   const products = getDataFromLocaleStorage();
-  const filteredData = products.filter((product) => product.id !== id);
+  const updateDeletedData = products.filter(product => product.id !== id);
+  saveToLocaleStorage(updateDeletedData);
+  renderProducts()
+}
 
-  saveToLocaleStorage(filteredData);
-  renderProducts();
-};
-
-
-sortMengeButton.addEventListener("click", () => {
+sortMengeBtn.addEventListener('click',()=>{
   const products = getDataFromLocaleStorage();
 
-  products.sort((a, b) => (isMengeAsc ? a.menge - b.menge : b.menge - a.menge));
-  isMengeAsc = !isMengeAsc;
+  iconDesc.classList.toggle('active')
+  iconAsc.classList.toggle('deactive')
 
-  saveToLocaleStorage(products);
-  renderProducts();
+ if (sortDirection==='asc') {
+  products.sort((a,b)=>a.menge-b.menge );
+  sortDirection='desc';
+ }else{
+ 
+ products.sort((a,b)=> b.menge-a.menge);
+  sortDirection='asc'
+ }
+
+renderProducts(products)  
 });
